b63b9d523d08179742d79c1e412db627ff750726
Pradeep Gowda <pradeep@btbytes.com>
2010-10-23 13:47:56 -0400

    better pdf output

--- markdown2pdf/bstringlib.sh
+++ markdown2pdf/bstringlib.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-curl http://bstring.cvs.sourceforge.net/viewvc/bstring/tree/bstrlib.txt\?pathrev\=HEAD -o bstrlib.txt
-sed -i 's/===============================================================================//g' bstrlib.txt
-pandoc bstrlib.txt -f markdown -t context --template=lesspaper-ltr.tex > bstrlib.tex
-texexec bstrlib.tex
-
+curl -s http://bstring.cvs.sourceforge.net/viewvc/bstring/tree/bstrlib.txt\?pathrev\=HEAD -o bstringlib.txt 
+sed -i 's/===============================================================================//g' bstringlib.txt
+pandoc  bstringlib.txt -f markdown -t context --template=lesspaper-ltr.tex > bstringlib.tex 
+texexec --pdf --silent --purgeall bstringlib.tex 
